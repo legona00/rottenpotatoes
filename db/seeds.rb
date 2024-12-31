3d253d8ea478d64e7cbdcd30e4e0bccc764f3e93
Leo Gonzalez <legon@DESKTOP-FT3UQ3V>
2024-12-31 11:34:24 +0800

    Added 3 movies to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,13 @@ more_movies = [
   {:title => 'Nomadland', :rating => 'R',
     :release_date => '19-Feb-2021'},
   {:title => 'CODA', :rating => 'PG-13',
-    :release_date => '13-Aug-2021'}
+  :release_date => '13-Aug-2021'},
+  {:title => 'Spiderman', :rating => 'PG-13',
+  :release_date => '1-Aug-2022'},
+  {:title => 'Batman', :rating => 'PG-13',
+  :release_date => '13-Oct-2023'},
+  {:title => 'Deadpool', :rating => 'R',
+    :release_date => '13-July-2024'},
 ]
 
 more_movies.each do |movie|
